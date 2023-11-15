5a3bd7605edca37c636e7a4bb8ae0619edc2033b
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-14 19:12:26 -0800

    added first question math.min function

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 // Examples:
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
-function min (v, y){
-let x = Math.min(v, y);
+function min(x, y){
+    return Math.min(x, y);
 }
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-
+function 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
